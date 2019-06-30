bb616a9ce57eed0f486887e00e4658c3d60483e5
Billy Levin <billylevs@gmail.com>
2019-06-30 21:28:53 +0100

    try to fix CORS

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -32,7 +32,9 @@ const startServer = async () => {
     cors({
       credentials: true,
       origin:
-        process.env.NODE_ENV === 'production' ? 'https://gymtracker.xyz' : 'http://localhost:3000',
+        process.env.NODE_ENV === 'production'
+          ? 'https://www.gymtracker.xyz'
+          : 'http://localhost:3000',
     }),
   );
 
