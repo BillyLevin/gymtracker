db6229314208d42eb4a55752c2a9329cbc287b06
Billy Levin <billylevs@gmail.com>
2019-07-01 23:20:55 +0100

    configure webpack to stop error resolving 'net' module

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,18 @@
 const withImages = require('next-images');
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require('@zeit/next-sass');
-module.exports = withImages(withTypescript(withSass()));
+module.exports = withImages(
+  withTypescript(
+    withSass({
+      webpack(config, options) {
+        config.node = {
+          ...(config.node || {}),
+          net: 'empty',
+          tls: 'empty',
+          dns: 'empty',
+        };
+        return config;
+      },
+    }),
+  ),
+);
