1423463b1a677498886c96054f76ac21da94bb6e
Billy Levin <billylevs@gmail.com>
2019-07-01 11:44:19 +0100

    just praying it somehow works at this point

--- packages/server/src/redis.ts
+++ packages/server/src/redis.ts
@@ -1,4 +1,4 @@
 import * as Redis from 'ioredis';
 
 export const redis =
-  process.env.NODE_ENV === 'production' ? new Redis(6379, process.env.REDDIS_HOST) : new Redis();
+  process.env.NODE_ENV === 'production' ? new Redis('redis://redis:6379') : new Redis();
